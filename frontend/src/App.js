8d0fdce95b822534b60c056000ce27252e0e993b
preyansh98 <preyansh98@gmail.com>
2020-05-07 23:09:20 -0400

    router set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LandingPage from './components/LandingPage/LandingPage';
+import TestPage from './TestPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render(){
+    return (
+      <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LandingPage}/>
+          <Route path="/test" component={TestPage}/>
+        </Switch>
+      </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'; 
+import { Link } from 'react-router-dom';
+
+export default class LandingPage extends Component {
+    state = {
+
+    }
+
+    render(){
+        return(
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
