8fdf74ed699064f2be27e74f201cd095d6861ad6
preyansh98 <preyansh98@gmail.com>
2020-05-08 00:26:20 -0400

    landing is a beauty

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,33 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LandingPage from './components/LandingPage/LandingPage';
 import TestPage from './TestPage';
+import Particles from "react-particles-js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 export default class App extends Component {
   render(){
     return (
       <div>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={LandingPage}/>
-          <Route path="/test" component={TestPage}/>
-        </Switch>
-      </Router>
+        <Router>
+          <Particles className="particles" params={particlesOptions} /> 
+          <Switch>
+            <Route exact path = "/" component = {LandingPage}/>
+            <Route path = "/test" component = {TestPage} />
+          </Switch>
+        </Router>
       </div>
-    );
+    );      
   }
 }
--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,16 +1,93 @@
-import React, {Component} from 'react'; 
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Card, Button, Jumbotron } from "react-bootstrap";
+import { FaGoogle } from "react-icons/fa";
+import { MdUnfoldMore, MdHelp, MdFeedback } from "react-icons/md";
+import { Fab, Action } from "react-tiny-fab";
+import "react-tiny-fab/dist/styles.css";
+import config from '../../config';
 
 export default class LandingPage extends Component {
-    state = {
+  state = {};
 
-    }
+  render() {
+    return (
+      <div>
+        <center>
+          <Card bg="dark" text="white" style={styles.cardStyle}>
+            <Card.Body>
+              <Card.Title>Welcome to Email Notifier!</Card.Title>
+              <br />
 
-    render(){
-        return(
-            <div>
-                Hello
-            </div>
-        )
-    }
+              <Card.Text>
+                Tired of constantly refreshing your inbox waiting for an important email? 
+              </Card.Text>
+            <Card.Text>
+                Email Notifier lets you receive real-time updates about specific emails by sending you an automated Phone Call and SMS Message. 
+                Use Email Notifier and never worry about missing important updates anymore.
+                </Card.Text>
+              <br />
+              <br />
+              <br />
+              <Button variant="light" size="lg" href={config.GOOGLE_OAUTH_HREF}>
+                <FaGoogle size="18px" /> | Sign In With Google
+              </Button>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">
+                Github Link, Fork Status, Stars etc 
+              </small>
+            </Card.Footer>
+          </Card>
+
+          <Fab
+            mainButtonStyles={styles.fab.mainButtonStyles}
+            actionButtonStyles={styles.fab.actionButtonStyles}
+            position={styles.fab.position}
+            icon={<MdUnfoldMore />}
+            event="hover"
+          >
+            <Action
+              style={styles.fab.actionButtonStyles}
+              text="Help"
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+            >
+              <MdHelp />
+            </Action>
+            <Action
+              style={styles.fab.actionButtonStyles}
+              text="Give Feedback"
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+            >
+              <MdFeedback />
+            </Action>
+          </Fab>
+        </center>
+      </div>
+    );
+  }
 }
+
+const styles = {
+  cardStyle: {
+    width: "40%",
+    height: "500px",
+    display: "flex",
+    marginTop: "10%",
+  },
+  fab: {
+    position: {
+      bottom: 10,
+      right: 10,
+    },
+    mainButtonStyles: {
+      backgroundColor: "#27ae60",
+    },
+    actionButtonStyles: {
+      backgroundColor: "#16a085",
+    },
+  },
+};
