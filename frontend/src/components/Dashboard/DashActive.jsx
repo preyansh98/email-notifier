592fc18782c7572086cf377e0564f093e9a9ee7f
preyansh98 <preyansh98@gmail.com>
2020-05-09 00:12:32 -0400

    finalize routing logic

---
+++ frontend/src/components/Dashboard/DashActive.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'; 
+
+export default class DashActive extends Component { 
+    state = {
+
+    }
+
+    render(){
+        return(
+            <div>
+                User with active job - Welcome back!
+            </div>
+        )
+    }
+}
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,9 @@
-const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:5000"
+const BACKEND_BASE_URL = process.env.BACKEND_BASE_URL || "http://localhost:5000"
 
 export default {
-    GOOGLE_OAUTH_HREF : BACKEND_URL + "/oauth-start",
+    GOOGLE_OAUTH_HREF : BACKEND_BASE_URL + "/oauth-start",
+
+    backendURLs : {
+        fetchUser : BACKEND_BASE_URL + "/user/fetch" 
+    }
 };
