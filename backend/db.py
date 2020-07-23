11069961e893ec530ca7abf063901f21e08e992f
preyansh98 <preyansh98@gmail.com>
2020-07-22 22:46:43 -0400

    move mongo config to OS env

--- backend/db.py
+++ backend/db.py
@@ -9,7 +9,9 @@ def initialize_and_return_db_client():
     if client is not None:
         return client
 
-    mongo_str = "mongodb+srv://admin:adminpassword@email-notifier-db-snhkt.azure.mongodb.net/test?retryWrites=true&w=majority"
-    client = MongoClient(mongo_str)
+    mongo_str = os.getenv('mongo_str')
+    
+    if mongo_str:
+        client = MongoClient(mongo_str)
 
     return client 
