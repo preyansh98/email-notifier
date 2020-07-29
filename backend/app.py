e0610b85b445df024ccf362c346ed03c6226b435
preyansh98 <preyansh98@gmail.com>
2020-07-28 22:24:56 -0400

    process webhook event from cloud pub sub

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,21 @@
 from flask import Flask, request, jsonify
+import json
+import base64
 app = Flask(__name__)
 
+# process webhook from cloud pubsub
 @app.route('/cloud/webhook/', methods=['POST'])
 def post_placeholder():
     req_data = request.get_json()
-    print(req_data)
-    return {'status' : 'success'}, 200   
+    notif_payload = req_data['message']['data']
+    
+    decoded = base64.b64decode(notif_payload).decode('utf-8')
+    resp_data = json.loads(decoded)
+    
+    email_address = resp_data['emailAddress']
+    history_id = resp_data['historyId']
+
+    
 
 @app.route('/')
 def index():
